fd2ad4ac52ca310a967acf707db5468e5fe8db31
HarrisZhang <harriszhang@live.cn>
2016-06-20 07:58:06 +0800

    init project

---
+++ CommonLoggingDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCommonLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog logger = LogManager.GetLogger(typeof(Program).FullName);
+
+            try
+            {
+                throw new Exception("This is a test.");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+
+            Console.Read();
+        }
+    }
+}
